a86448e7a0b960a8d2c1f3d0e097817af5940249
KANISHK N <kanishku736@gmail.com>
2026-08-16 22:45:54 +0530

    Add Netlify URL to allowed origins

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const app = express();
 
 const allowedOrigins = [
   process.env.CLIENT_ORIGIN,
+  'https://recipebox-site.netlify.app',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
